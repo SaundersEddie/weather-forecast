b29a5bc22b4f17f83449ecd8241c5d3bc11835a1
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-10 09:05:41 -0500

    installed react-bootstrap created base header and main body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import WeatherNavbar from './components/Navbar/WeatherNavbar';
+import DisplayWeather from './components/DisplayWeather/DisplayWeather';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <WeatherNavbar />
+      <DisplayWeather />
     </div>
   );
 }
---
+++ src/components/DisplayWeather/DisplayWeather.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DisplayWeather.css';
+
+const DisplayWeather = () => {
+    return (
+        <h1>Display Weather</h1>
+    )
+}
+
+export default DisplayWeather;
